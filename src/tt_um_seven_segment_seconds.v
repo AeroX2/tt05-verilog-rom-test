77b7d25873ea343c60eecd958affb632b7d11cd2
AeroX2 <james@ridey.email>
2023-10-24 12:07:54 +1100

    Test2

--- src/tt_um_seven_segment_seconds.v
+++ src/tt_um_seven_segment_seconds.v
@@ -14,11 +14,11 @@ module tt_um_seven_segment_seconds #( parameter MAX_COUNT = 24'd10_000_000 ) (
 
 	reg [15:0] i;
 	reg [15:0] pc;
-	reg [7:0] rom [0:65535];
+	reg [7:0] rom [0:255];
 
 	always @ (posedge rst)
 	begin
-		for (i = 0; i < 65535; i = i + 1)
+		for (i = 0; i < 255; i = i + 1)
 			rom[i] = 8'b00000000;
 	end
 
